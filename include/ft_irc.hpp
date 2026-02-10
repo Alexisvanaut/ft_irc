ef5232a91d7cd20062d05d6f2e4fa15d63eb32f1
Alexis Vanautgaerden <alvanaut@fu-r10-p5.s19.be>
2026-02-10 12:56:33 +0100

    Add: mandatory libs

---
+++ include/ft_irc.hpp
@@ -0,0 +1,19 @@
+#ifndef FT_IRC_HPP
+# define FT_IRC_HPP
+
+# include <iostream>
+# include <string>
+# include <sstream>
+# include <vector>
+
+# include <sys/socket.h>
+# include <netinet/in.h>
+# include <arpa/inet.h>
+# include <netdb.h>
+# include <signal.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/stat.h>
+# include <poll.h>
+
+#endif
